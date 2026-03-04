b1fc5b5affd25d07e3274f6d92aac67558d341e6
asyrafabdzawawi <66741161+asyrafabdzawawi@users.noreply.github.com>
2026-03-04 22:12:42 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, redirect, render_template
+import gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+# Google API scope
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# load credentials
+creds = Credentials.from_service_account_file(
+    "credentials.json",
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+# buka Google Sheet
+sheet = client.open("PBD_FORM").sheet1
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    form = request.form
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    row = [
+        timestamp,
+        form.get("nama"),
+        form.get("jawatan"),
+        form.get("mp"),
+        form.get("tahun"),
+        form.get("1.1"),
+        form.get("2.1"),
+        form.get("2.2"),
+        form.get("3.1"),
+        form.get("3.2"),
+        form.get("3.3"),
+        form.get("3.4"),
+        form.get("3.5"),
+        form.get("4.1"),
+        form.get("4.2"),
+        form.get("4.3"),
+        form.get("5.1"),
+        form.get("5.2"),
+        form.get("5.3"),
+        form.get("5.4"),
+        form.get("5.5"),
+        form.get("5.6"),
+        form.get("1.1_b"),
+        form.get("1.2_b"),
+        form.get("1.3_b"),
+        form.get("1.4_b"),
+        form.get("1.5_b"),
+        form.get("1.6_b"),
+        form.get("1.7_b"),
+        form.get("1.8_b"),
+        form.get("1.9_b"),
+        form.get("1.10_b"),
+        form.get("1.11_b")
+    ]
+
+    sheet.append_row(row)
+
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
